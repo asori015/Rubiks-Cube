7d10e64425a9ac8fabef4c206e2d75da4b902186
Anthony Soria Limon <asori015@hammer.cs.ucr.edu>
2017-10-03 22:08:01 -0700

    added user input

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-int main()
+#include <iostream>
+#include <string>
+
+int main(){
+	std::string name;
+	std::cout << " What is your name?" << std::endl;
+	std::cin >> name;
+	std::cout << "Hello " << name << "!" << std::endl;
+
+	return 1;
+}
